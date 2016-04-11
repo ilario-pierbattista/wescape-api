fc561e82adc8d06b9346e060b7587d3cbb73d7c4
Ilario Pierbattista <pierbattista.ilario@gmail.com>
2016-04-11 23:34:52 +0200

    composer

--- wescape/web/app_dev.php
+++ wescape/web/app_dev.php
@@ -10,6 +10,7 @@ umask(0000);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+// @TODO Attualmente il controllo dell'ambiente è rimosso. App_dev è accessibile ovunque
 /*if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
