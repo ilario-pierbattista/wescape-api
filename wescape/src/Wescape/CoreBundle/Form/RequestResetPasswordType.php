abe00c7f5867f2db0301e0376f2fd5e156dab5a5
Ilario Pierbattista <pierbattista.ilario@gmail.com>
2016-04-25 16:03:46 +0200

    Agiornamento gitignore

---
+++ wescape/src/Wescape/CoreBundle/Form/RequestResetPasswordType.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ilario
+ * Date: 25/04/16
+ * Time: 14.01
+ */
+
+namespace Wescape\CoreBundle\Form;
+
+
+class RequestResetPasswordType
+{
+
+}
---
+++ wescape/src/Wescape/CoreBundle/Service/PasswordResetService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ilario
+ * Date: 25/04/16
+ * Time: 14.34
+ */
+
+namespace Wescape\CoreBundle\Service;
+
+
+class PasswordResetService
+{
+
+}
