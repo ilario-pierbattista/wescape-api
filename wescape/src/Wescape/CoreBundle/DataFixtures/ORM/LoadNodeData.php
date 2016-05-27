553cd48361bf2ba45fdaca36c3484813d7fe1dd7
Ilario Pierbattista <pierbattista.ilario@gmail.com>
2016-05-27 18:25:16 +0200

    optimized imports

--- wescape/src/Wescape/CoreBundle/DataFixtures/ORM/LoadNodeData.php
+++ wescape/src/Wescape/CoreBundle/DataFixtures/ORM/LoadNodeData.php
@@ -3,7 +3,6 @@
 namespace Wescape\CoreBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
-use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Wescape\CoreBundle\Entity\Node;
